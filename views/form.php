2d5b09dd3728c8914e6149c7cdb928a3215bf226
Moussa ALI MOUSSA <moussa.ali.pro@gmail.com>
2021-03-01 21:19:59 +0000

    Mise en place de la class form-label

--- views/form.php
+++ views/form.php
@@ -4,17 +4,16 @@
         <div class="card-body">
             <form method="<?php echo $formMethod ?>">
                 <div class="form-row pb-3 d-flex align-items-center">
-                    <label for="account" class="col-lg-3 text-lg-right">Email <span class="text-danger">*</span></label>
+                    <label for="account" class="col-lg-3 text-lg-right col-form-label">Email <span class="text-danger">*</span></label>
                     <input type="text" required disabled class="form-control col-6 col-lg-5" id="account" value="<?php echo $account ?>"><span class="col-6 col-lg-4">@<?php echo $domain ?></span>
-                    <!-- <input type="hidden" name="account" <?php //if($action == 'show') echo 'disabled'; ?> value="<?php //echo $account ?>"> -->
                 </div>
 
                 <div class="form-row pb-3">
-                    <label class="col-lg-3 text-lg-right" for="content">Message <span class="text-danger">*</span></label>
+                    <label class="col-lg-3 col-form-label text-lg-right" for="content">Message <span class="text-danger">*</span></label>
                     <textarea class="col-lg-9 form-control" required <?php if($action == 'show') echo 'disabled'; ?> rows="8" id="content" name="content"><?php echo $content ?></textarea>
                 </div>
                 
-                <div class="offset-lg-3 form-check pb-3">
+                <div class="offset-lg-3 form-check pb-3 d-flex align-items-center">
                     <input class="form-check-input" type="checkbox" <?php if($action == 'show') echo 'disabled'; else echo 'checked'; ?> id="copy" name="copy" value="<?php echo $copy ?>">
                     <label class="form-check-label" for="copy">
                         Sauvegarder le mail reçu
@@ -22,12 +21,12 @@
                 </div>
 
                 <div class="form-row pb-3">
-                    <label class="col-lg-3 text-lg-right" for="from">Début <span class="text-danger">*</span></label>
+                    <label class="col-lg-3 col-form-label text-lg-right" for="from">Début <span class="text-danger">*</span></label>
                     <input type="date" required <?php if($action == 'show') echo 'disabled'; ?> class="col-lg-9 form-control" id="from" name="from" value="<?php echo $from ?>">
                 </div>
 
                 <div class="form-row pb-3">
-                    <label class="col-lg-3 text-lg-right" for="to">to <span class="text-danger">*</span></label>
+                    <label class="col-lg-3 col-form-label text-lg-right" for="to">Fin (inclus) <span class="text-danger">*</span></label>
                     <input type="date" required <?php if($action == 'show') echo 'disabled'; ?> class="col-lg-9 form-control" id="to" name="to" value="<?php echo $to ?>">
                 </div>
 
