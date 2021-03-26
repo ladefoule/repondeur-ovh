e77e374cb083dc91d2bdf8fe737b89ad62d129dc
ALI MOUSSA Moussa <moussa.ali.pro@gmail.com>
2021-03-26 12:54:49 +0000

    variable consumer_key => consumerKey

--- config.example.php
+++ config.example.php
@@ -2,7 +2,7 @@
     // GET DEL POST 7 DAYS resp/*
     $applicationKey = "Nti8t2zEjJ";
     $applicationSecret = "RFBZQVWugQY6y8A5tH";
-    $consumer_key = "2MFu1ri4NgzZLQa3uZg";
+    $consumerKey = "2MFu1ri4NgzZLQa3uZg";
 
     $endpoint = 'ovh-eu';
     $domain = "example.fr";
